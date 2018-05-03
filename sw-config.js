12afe1b2bec03f793aa6c1d9b1704ba65ff64de7
Rafael Guerra <faelwar@gmail.com>
2018-05-03 12:00:32 -0300

    SW

---
+++ sw-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    staticFileGlobs: [
+      'docs/css/**.css',
+      'docs/**/**.html',
+      'docs/**/**.jpg',
+      'docs/**/**.png',
+      'docs/js/**.js'
+    ],
+    stripPrefix: 'docs',
+  }
